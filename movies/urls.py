e3242d9c773e6b800944d4a8afe4f970fc7ab467
weshy <josephwaweru96@gmail.com>
2022-06-04 15:05:13 +0300

    login and redister views and urls

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('', views.home, name='home'),
-    
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+from .forms import CreateUserForm, LoginUserForm
 
 # Create your views here.
-def home(request):
-    return render(request, 'base.html')
+def register(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+    if request.method =='POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            messages.success(request, f'Account created for {username}')
+
+            return redirect('login')
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = LoginUserForm()
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html', {'form':form})
+
+    
